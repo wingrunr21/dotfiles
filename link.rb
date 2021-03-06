39df495e5a40d367dd3a64f29ab96fd85b16dfe1
Stafford Brunk <wingrunr21@gmail.com>
2021-03-05 19:06:08 -0700

    Drop link.rb

--- link.rb
+++
@@ -1,7 +0,0 @@
-require 'fileutils'
-
-FILES = %w[gemrc gitconfig gitignore_global irbrc zshrc zshenv zprofile zimrc zim zfunctions]
-Dir.chdir(File.join(ENV['HOME'], 'dotfiles'))
-FILES.each do |dotfile|
-  FileUtils.ln_s File.join(Dir.pwd, dotfile), File.join(ENV['HOME'], ".#{dotfile}"), :force => true
-end
